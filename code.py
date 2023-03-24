19306a603d15cbef78d8bf4292ae02bf2b1f5703
Muhammad Hur Khan <122796904+MuhammadHurKhan@users.noreply.github.com>
2023-03-24 18:00:38 +0500

    Update code.py

--- code.py
+++ code.py
@@ -77,5 +77,5 @@ def get_portfolio_allocation(symbols_list, initial_investment):
         st.write("$", round(portfolio_value, 2))
     else: 
         st.write("Please enter valid company names and corresponding shares")
- get_portfolio_allocation(symbols_list, initial_investment)
+get_portfolio_allocation(symbols_list, initial_investment)
     
