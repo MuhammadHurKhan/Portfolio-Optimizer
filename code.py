5a5f5d2dd3d89f238fd4fd6a014ff9c44959d690
Muhammad Hur Khan <122796904+MuhammadHurKhan@users.noreply.github.com>
2023-03-24 16:49:11 +0500

    Update code.py

--- code.py
+++ code.py
@@ -4,43 +4,40 @@ from datetime import date
 from pypfopt import expected_returns, risk_models, EfficientFrontier
 import plotly.graph_objs as go
 
-
 # Set app title
-st.title("Optimal Portfolio Allocation")
-
+st.title("Portfolio Performance Comparison")
 
 # Create search bar
-st.sidebar.subheader("Search for a company")
-symbol = st.sidebar.text_input("Ticker symbol (e.g. AAPL)")
-
-
-# Load data for selected company
-today = date.today().strftime('%Y-%m-%d')
-if symbol:
-    stock = yf.Ticker(symbol)
-    df = stock.history(period="max")[['Close']]
-    df = df.rename(columns={'Close': symbol})
-
+st.sidebar.subheader("Add stocks to your portfolio")
+symbols = st.sidebar.text_input("Enter ticker symbols separated by commas (e.g. AAPL,MSFT,AMZN)").upper()
+symbols_list = [symbol.strip() for symbol in symbols.split(",")]
 
 # Create input fields for user
-st.sidebar.subheader("Portfolio Parameters")
-initial_investment = st.sidebar.number_input("Initial investment amount ($)", value=100000, step=1000)
-target_return = st.sidebar.number_input("Target annualized return (%)", value=10.0, step=0.5)
-risk = st.sidebar.number_input("Desired annualized volatility (%)", value=20.0, step=0.5)
+st.sidebar.subheader("Number of shares for each stock")
+shares_list = []
+for symbol in symbols_list:
+    shares = st.sidebar.number_input(f"{symbol} shares", value=100, step=1)
+    shares_list.append(shares)
 
+initial_investment = st.sidebar.number_input("Initial investment amount ($)", value=100000, step=1000)
+
+# Load data for selected companies
+today = date.today().strftime('%Y-%m-%d')
+data = yf.download(symbols_list, start="2010-01-01", end=today, group_by='ticker')['Close']
+df = data.dropna()
 
 # Optimize portfolio
-if symbol:
+if len(df.columns) > 0:
     # Calculate expected returns and covariance matrix
     mu = expected_returns.mean_historical_return(df)
     S = risk_models.sample_cov(df)
 
     ef = EfficientFrontier(mu, S)
-    weights = ef.efficient_return(target_return/100, market_neutral=True)
+    weights = ef.max_sharpe()
 
     # Calculate allocation of shares
-    latest_prices = stock.history(period="1d")['Close'].values[0]
-    allocation = {s: round(weights[s] * initial_investment / latest_prices, 2) for s in weights.keys()}
+    latest_prices = df.iloc[-1]
+    allocation = {s: round(weights[s] * initial_investment / latest_prices[i], 2) for i, s in enumerate(weights.keys())}
 
     # Calculate portfolio statistics
     portfolio_value = sum([allocation[s] * latest_prices[i] for i, s in enumerate(allocation.keys())])
@@ -50,16 +47,25 @@ if symbol:
     annualized_volatility = (cov_matrix.mul(allocation, axis=0).mul(allocation, axis=1).sum().sum() * 252) ** 0.5
     sharpe_ratio = (annualized_return - 0.02) / annualized_volatility
 
+    # Calculate S&P 500 and NASDAQ annual returns
+    sp500 = yf.Ticker('^GSPC')
+    sp500_hist = sp500.history(start="2010-01-01", end=today)
+    sp500_annual_return = (sp500_hist['Close'][-1] / sp500_hist['Close'][0]) ** (1/len(sp500_hist)) - 1
+
+    nasdaq = yf.Ticker('^IXIC')
+    nasdaq_hist = nasdaq.history(start="2010-01-01", end=today)
+    nasdaq_annual_return = (nasdaq_hist['Close'][-1] / nasdaq_hist['Close'][0]) ** (1/len(nasdaq_hist)) - 1
+
     # Visualize portfolio performance
     portfolio_returns = (df.pct_change() * allocation).sum(axis=1)
     cumulative_returns = (1 + portfolio_returns).cumprod()
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=cumulative_returns.index, y=cumulative_returns.values, name='Portfolio'))
-    fig.add_trace(go.Scatter(x=cumulative_returns.index, y=(1 + df.pct_change().mean(axis=1)).cumprod().values, name='Benchmark'))
+    fig.add_trace(go.Scatter(x=cumulative_returns.index, y= (1 + df['SP500'].pct_change().mean(axis=1)).cumprod().values, name='S&P 500'))
+    fig.add_trace(go.Scatter(x=cumulative_returns.index, y=(1 + df['NASDAQ'].pct_change().mean(axis=1)).cumprod().values, name='NASDAQ'))
     fig.update_layout(title='Portfolio Performance', xaxis_title='Date', yaxis_title='Cumulative Returns')
     st.plotly_chart(fig, use_container_width=True)
-
+    
     # Show allocation of shares
     st.subheader("Allocation of Shares")
     for symbol, shares in allocation.items():
@@ -72,5 +78,3 @@ if symbol:
     st.write("Annualized Volatility: {:.2%}".format(annualized_volatility))
     st.write("Sharpe Ratio: {:.2f}".format(sharpe_ratio))
 
-else:
-    st.error("Error occurred during optimization: {}".format(str(e)))
